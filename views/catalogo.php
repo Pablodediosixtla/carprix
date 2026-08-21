1b9eb6a4f0e220b54875814fb0de076ea39631b8
Pablo de Dios <4855797@gmail.com>
2026-08-21 13:35:47 -0600

    Ampliar ancho de catalogo cliente

--- views/catalogo.php
+++ views/catalogo.php
@@ -12,7 +12,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/styles.css?v=20260803-3">
-    <link rel="stylesheet" href="../css/catalogo.css?v=20260821-1">
+    <link rel="stylesheet" href="../css/catalogo.css?v=20260821-2">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body class="bg-dark">
